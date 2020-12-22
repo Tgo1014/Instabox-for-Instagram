f67527a0eb5f4276a7843f348703e4ccfb11cadb
Tiago Araujo de Souza <tiar@nodesagency.com>
2020-12-22 23:00:23 +0100

    Added back Timber

--- app/src/main/java/tgo1014/instabox/App.kt
+++ app/src/main/java/tgo1014/instabox/App.kt
@@ -2,6 +2,16 @@ package tgo1014.instabox
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
