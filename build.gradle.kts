49140a782e01d16adcac80d37baabb27036b6ed8
Tiago Araujo de Souza <tiar@nodesagency.com>
2020-12-27 21:59:37 +0100

    Migrated from Groovy to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.30-alpha")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
